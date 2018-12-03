f04411e54c0e870199d1303654884b160fe3933f
Olivier Perez <operez@groupeseb.com>
2018-12-03 13:58:06 +0100

    Rename DSL annotation

--- src/main/kotlin/fr/o80/week2/lib/injection.dsl.kt
+++ src/main/kotlin/fr/o80/week2/lib/injection.dsl.kt
@@ -1,26 +1,26 @@
 package fr.o80.week2.lib
 
 @DslMarker
-annotation class Injection
+annotation class InjectionDsl
 
-@Injection
+@InjectionDsl
 inline fun module(block: InjectModule.() -> Unit) = InjectModule().apply(block)
 
-@Injection
+@InjectionDsl
 inline fun <reified T : Any> InjectModule.factory(noinline generator: () -> T) {
     val className = T::class.qualifiedName
             ?: throw IllegalArgumentException("A non-real class cannot be injected")
     generators[className] = Prototype(generator)
 }
 
-@Injection
+@InjectionDsl
 inline fun <reified T : Any> InjectModule.singleton(noinline generator: () -> T) {
     val className = T::class.qualifiedName
             ?: throw IllegalArgumentException("A non-real class cannot be injected")
     generators[className] = Singleton(className, generator)
 }
 
-@Injection
+@InjectionDsl
 inline fun <reified T : Any> InjectModule.scoped(noinline generator: () -> T) {
     val className = T::class.qualifiedName
             ?: throw IllegalArgumentException("A non-real class cannot be injected")
