8c24cc9ce45fcf2d22143307ca45dae3646816da
Olivier PEREZ <olivier@olivierperez.fr>
2018-12-15 17:40:01 +0100

    Prepare day 15

---
+++ src/main/kotlin/fr/o80/day15/day15.kt
@@ -0,0 +1,17 @@
+package fr.o80.day15
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    measureTimeMillis { println("15.1 => ${exercise_15_1()}") }.also { println("Time: ${it}ms\n") }
+    measureTimeMillis { println("15.2 => ${exercise_15_2()}") }.also { println("Time: ${it}ms\n") }
+}
+
+fun exercise_15_1(): Int {
+    return 0
+}
+
+fun exercise_15_2(): Int {
+    return 0
+
+}
