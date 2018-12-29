6e14185f9add6196236df97066ae51af5aaec026
Olivier Perez <operez@groupeseb.com>
2018-12-29 18:57:18 +0100

    Week 4 - Allow to mock functions bodies

--- src/main/kotlin/fr/o80/week4/week4.kt
+++ src/main/kotlin/fr/o80/week4/week4.kt
@@ -1,5 +1,6 @@
 package fr.o80.week4
 
+import java.lang.IllegalArgumentException
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
@@ -8,27 +9,43 @@ object MockTool {
     var recording = false
         private set
 
-    private var value: Any? = null
+    private var value: MockedBody? = null
 
-    fun record(v: Any) {
+    fun record(v: MockedBody) {
         value = v
         recording = true
     }
 
-    fun get(): Any {
+    fun get(): MockedBody {
         recording = false
         return value.also { value = null } ?: throw IllegalStateException("Nothing is recording")
     }
 }
 
-class MockHandler : InvocationHandler {
-    private val values = mutableMapOf<Method, Any>()
+typealias MockedBody = () -> Any?
 
-    override fun invoke(mock: Any, method: Method, args: Array<out Any>?): Any {
+class MockHandler : InvocationHandler {
+    private val values = mutableMapOf<Method, MockedBody>()
+
+    override fun invoke(mock: Any, method: Method, args: Array<out Any>?): Any? {
         if (MockTool.recording) {
             values[method] = MockTool.get()
+            return defaultValueFor(method.returnType)
+        } else {
+            val value = values[method]!!
+            return value()
         }
-        return values[method]!!
+    }
+
+    private fun defaultValueFor(returnType: Class<*>?): Any? = when(returnType) {
+        Byte::class.java -> 0.toByte()
+        Char::class.java -> 0.toChar()
+        Int::class.java -> 0
+        Long::class.java -> 0L
+        Float::class.java -> 0f
+        Double::class.java -> 0f.toDouble()
+        Function::class.java -> {}
+        else -> throw IllegalArgumentException("Return type \"$returnType\" not mockable, because I'm lazy.")
     }
 
 }
@@ -40,6 +57,11 @@ inline fun <reified T> mock(): T {
 }
 
 inline fun <T : Any> setReturnValue(block: () -> T, value: T) {
+    MockTool.record { value }
+    block()
+}
+
+inline fun <T : Any> setBody(block: () -> T, noinline value: MockedBody) {
     MockTool.record(value)
     block()
 }
--- src/test/kotlin/fr/o80/week4/Week4UnitTest.kt
+++ src/test/kotlin/fr/o80/week4/Week4UnitTest.kt
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 interface Example {
+    fun getChar(): Char
     fun getInt(): Int
+    fun getLong(): Long
+    fun getFloat(): Float
+    fun getDouble(): Double
 }
 
 class Week4UnitTest {
@@ -12,16 +16,18 @@ class Week4UnitTest {
     @Test
     fun shouldMockIt() {
         val a = mock<Example>()
+        setReturnValue({ a.getChar() }, '2')
         setReturnValue({ a.getInt() }, 2)
+        setReturnValue({ a.getLong() }, 2)
+        setReturnValue({ a.getFloat() }, 2)
+        setReturnValue({ a.getDouble() }, 2)
         assertEquals(2, a.getInt())
 
-        /*
         var i = 1
-        val b =mock<Example>()
-        setBody({b.getInt()}, { i++ })
+        val b = mock<Example>()
+        setBody({ b.getInt() }, { i++ })
         assertEquals(1, b.getInt())
         assertEquals(2, b.getInt())
         assertEquals(3, b.getInt())
-        */
     }
 }
