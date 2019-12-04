c0274db77a4eccadc6267563e0a7ce5084fa324f
Olivier Perez <olivier@olivierperez.fr>
2019-12-04 16:18:06 +0100

    Improve model

--- src/test/java/fr/o80/code2019/model/DayXTest.kt
+++ src/test/java/fr/o80/code2019/model/DayXTest.kt
@@ -1,14 +1,27 @@
 package fr.o80.code2019.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class DayXTest {
 
-    @Test
-    fun testBilly() {
-        val dayX = DayX()
-        assertEquals(42, dayX.goBilly(mutableListOf()))
+    @Nested
+    inner class PartOne {
+        @Test
+        fun testBilly() {
+            val dayX = DayX()
+            assertEquals(42, dayX.goBilly(mutableListOf()))
+        }
+    }
+
+    @Nested
+    inner class PartTwo {
+        @Test
+        fun testBilly() {
+            val dayX = DayX()
+            assertEquals(42, dayX.goBilly(mutableListOf()))
+        }
     }
 
 }
