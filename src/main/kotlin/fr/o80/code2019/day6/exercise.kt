321206e14cc0b98725cc364e84d3434a938ab8ba
Olivier Perez <olivier@olivierperez.fr>
2019-12-06 19:46:11 +0100

    Day6 - part 2

--- src/main/kotlin/fr/o80/code2019/day6/exercise.kt
+++ src/main/kotlin/fr/o80/code2019/day6/exercise.kt
@@ -7,6 +7,8 @@ fun main() {
         val day = Day6()
         val partOne = day.goBilly(day.parseInput(day6Input))
         println("PartOne: $partOne")
+        val partTwo = day.computePathToSanta(day.parseInput(day6Input))
+        println("PartTwo: $partTwo")
     }
 
     println("${time}ms")
@@ -41,18 +43,57 @@ class Day6 {
 
                 planet
             }
+
             planets.computeIfAbsent(instruction.orbit) { name ->
-                Planet(name)
+                Planet(name).apply { attractedBy = instruction.planet }
+            }
+            planets.computeIfPresent(instruction.orbit) { _, planet ->
+                planet.apply { attractedBy = instruction.planet }
             }
         }
         return planets
     }
 
+    fun computePathToSanta(input: List<Instruction>): Int {
+        val planets: Map<String, Planet> = buildPlanets(input)
+
+        val pathYOUtoCOM: List<String> = planets.pathToCOM("YOU")
+        val pathSANtoCOM: List<String> = planets.pathToCOM("SAN")
+
+        val pivot: String = common(pathYOUtoCOM, pathSANtoCOM)
+
+        return pathYOUtoCOM.indexOf(pivot) + pathSANtoCOM.indexOf(pivot)
+    }
+
+    private fun common(path1: List<String>, path2: List<String>): String {
+        for (p1 in path1) {
+            for (p2 in path2) {
+                if (p1 == p2)
+                    return p1
+            }
+        }
+        throw IllegalArgumentException("Tes 2 chemins n'ont pas de match !")
+    }
+
+}
+
+private fun Map<String, Planet>.pathToCOM(origin: String): List<String> {
+    val path = mutableListOf<String>()
+    var current = get(origin)
+    while (current != null) {
+        current = get(current.attractedBy)
+        if (current != null) {
+            path.add(current.name)
+        }
+    }
+    return path
 }
 
 class Instruction(val planet: String, val orbit: String)
 
 data class Planet(val name: String) {
+
+    var attractedBy: String? = null
 
     val orbits = mutableListOf<String>()
 
--- src/test/java/fr/o80/code2019/Day6Test.kt
+++ src/test/java/fr/o80/code2019/Day6Test.kt
@@ -3,15 +3,18 @@ package fr.o80.code2019
 import fr.o80.code2019.day6.Day6
 import fr.o80.code2019.day6.day6Input
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
+@DisplayName("Day 6 tests")
 internal class Day6Test {
 
     @Nested
+    @DisplayName("Day 6 Part 1")
     inner class PartOne {
         @Test
-        fun test_1() {
+        fun `example from AOC`() {
             val input = "COM)B\n" +
                 "B)C\n" +
                 "C)D\n" +
@@ -29,18 +32,39 @@ internal class Day6Test {
         }
 
         @Test
-        fun test_real() {
+        fun `from my puzzle input`() {
             val day = Day6()
             assertEquals(140608, day.goBilly(day.parseInput(day6Input)))
         }
     }
 
     @Nested
+    @DisplayName("Day 6 Part 2")
     inner class PartTwo {
         @Test
-        fun testBilly() {
+        fun `example from AOC`() {
+            val input_1 = "COM)B\n" +
+                "B)C\n" +
+                "C)D\n" +
+                "D)E\n" +
+                "E)F\n" +
+                "B)G\n" +
+                "G)H\n" +
+                "D)I\n" +
+                "E)J\n" +
+                "J)K\n" +
+                "K)L\n" +
+                "K)YOU\n" +
+                "I)SAN"
             val day = Day6()
-            assertEquals(42, day.goBilly(mutableListOf()))
+            assertEquals(4, day.computePathToSanta(day.parseInput(input_1)))
+        }
+
+        @Test
+        fun `from my puzzle input`() {
+            val input_1 = day6Input
+            val day = Day6()
+            assertEquals(337, day.computePathToSanta(day.parseInput(input_1)))
         }
     }
 
